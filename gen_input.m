e6cea5716430be933de53ccac988f0ff39f9ec31
James Kristoff <jskrist@gmail.com>
2025-12-02 09:55:13 -0500

    fixed bug caused by user canceling the file dialog.

--- gen_input.m
+++ gen_input.m
@@ -6,19 +6,21 @@ end
 
 filename = uiputfile('*.txt', 'input file name', ['input_day_', num2str(day), '.txt']);
 
-if(isempty(filename))
+if(isnumeric(filename))
     return
 end
 
 fid = fopen(filename, 'w');
 clean.file = onCleanup(@()fclose(fid));
 
-num_entries = 6;
-distances = strsplit(num2str(randi(80, 1, num_entries)));
-direction_options = 'RL';
-directions = cellstr(direction_options(randi(2, num_entries, 1))')';
-
-data = strcat(directions, distances);
-fprintf(fid, '%s\n', data{:});
+if(day <= 1)
+    num_entries = 6;
+    distances = strsplit(num2str(randi(80, 1, num_entries)));
+    direction_options = 'RL';
+    directions = cellstr(direction_options(randi(2, num_entries, 1))')';
+    
+    data = strcat(directions, distances);
+    fprintf(fid, '%s\n', data{:});
+end
 
 end
